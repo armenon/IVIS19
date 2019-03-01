47d2a25ff4204c473f54c60efd0ca1969e3bf6a7
Marco Koivisto <marcokoivisto@me.com>
2019-03-01 17:00:56 +0100

    Progress

--- src/components/variables-legend/VariablesLegend.jsx
+++ src/components/variables-legend/VariablesLegend.jsx
@@ -14,12 +14,27 @@ class VariablesLegend extends Component {
 	}
 	render() {
 		return (
-			<div className="variables-legend d-flex">
-				<span>Debt</span>
-				<span>vs</span>
-				<span>HDI</span>
-				<span>&</span>
-				<span>GDP</span>
+			<div className="variables-container">
+				<div className="variables box-shadow">
+					<span className="large">
+					<span className="label d-block">SIZE and %</span>
+					Debt
+					</span>
+					<div className="spacing-h small"></div>
+					<span className="label">vs</span>
+					<div className="spacing-h small"></div>
+					<span className="large">
+					<span className="label d-block">MAP COLOR</span>
+					HDI
+					</span>
+					<div className="spacing-h small"></div>
+					<span className="label">&</span>
+					<div className="spacing-h small"></div>
+					<span className="large">
+					<span className="label d-block">SIZE and %</span>
+					GDP
+					</span>
+				</div>
 			</div>
 		);
 	}
