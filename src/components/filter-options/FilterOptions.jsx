546117d217c07222fb93a8b4f5d76785951e502e
magnuslh <magnuslh@kth.se>
2019-03-02 18:33:13 +0100

    fixed bug in filteroptions

--- src/components/filter-options/FilterOptions.jsx
+++ src/components/filter-options/FilterOptions.jsx
@@ -12,49 +12,43 @@ import './FilterOptions.scss';
 const createSliderWithTooltip = Slider.createSliderWithTooltip;
 const Range = createSliderWithTooltip(Slider.Range);
 
-var marks = {
-    0: {
-        style: {
-            fontSize: 10,
-            color: "#A9A9A9",
-            marginLeft: 5
-        },
-        label: <strong>MIN</strong>
-    }
-  };
 
 
 
 class FilterOption extends Component {
-    
+
     constructor(props){
         super(props)
 
-        marks = { // I don't give a shit anymore
-            0: {
-                style: {
-                    fontSize: 10,
-                    color: "#A9A9A9",
-                    marginLeft: 5
-                },
-                label: <strong>MIN</strong>
-            }
-          };
+
       }
 
-    componentWillMount() {
-        var tempValue = this.props.maxFilterValue; // So we can dinamically use the max filter value
-        marks[tempValue] =  {
-            style: {
-              fontSize: 10,
-              color: "#A9A9A9",
-              marginLeft: -6
-            },
-            label: <strong>MAX</strong>,
-          }
-    }
+
 
     render() {
+
+      var marks = {
+          0: {
+              style: {
+                  fontSize: 10,
+                  color: "#A9A9A9",
+                  marginLeft: 5
+              },
+              label: <strong>MIN</strong>
+          }
+        };
+    const tempValue = this.props.maxFilterValue; // So we can dinamically use the max filter value
+    marks[tempValue] =  {
+        style: {
+          fontSize: 10,
+          color: "#A9A9A9",
+          marginLeft: -6
+        },
+        label: <strong>MAX</strong>,
+      }
+
+
+
         return (
         <div className = "filter-container">
             <h3 className = "filter-title">{this.props.filterName}</h3>
@@ -64,8 +58,8 @@ class FilterOption extends Component {
                     <h4 className = "filter-right-scale">{this.props.filterRightValue}</h4>
                 </div>
                 <div className = "slider-body">
-                    <Range marks={marks} trackStyle={[{ backgroundColor: "#A9A9A9" }]} railStyle={{ backgroundColor: "#343434" }} 
-                        step={this.props.step} min={this.props.minFilterValue} max={this.props.maxFilterValue} 
+                    <Range marks={marks} trackStyle={[{ backgroundColor: "#A9A9A9" }]} railStyle={{ backgroundColor: "#343434" }}
+                        step={this.props.step} min={this.props.minFilterValue} max={this.props.maxFilterValue}
                         handleStyle={{
                             borderColor: "#ffffff",
                             height: 19,
