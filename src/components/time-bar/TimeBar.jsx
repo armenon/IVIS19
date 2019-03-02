803afb9f1eb13c0894cf5c0d24dac58646277466
Marco Koivisto <marcokoivisto@me.com>
2019-03-02 13:17:58 +0100

    Ux shit

--- src/components/time-bar/TimeBar.jsx
+++ src/components/time-bar/TimeBar.jsx
@@ -3,19 +3,17 @@ import React, { Component } from 'react';
 import './TimeBar.scss';
 
 class TimeBar extends Component {
-
-
 	render() {
 		return (
 			<div className={this.props.isFull ? 'time-bar full' : 'time-bar'}>
-				<span className="label large color-dark">1990</span>
+				<span className="label large color-dark">2010</span>
 				<div className="spacing-h"></div>
 				<div className="input-holder">
-					<input onChange={this.props.onYearChange} type="range" name="year" className="form-control-range" min="1990" max="2017" />
-					<output for="year" onforminput="value = year.valueAsNumber;">{this.props.year}</output>
+					<input onChange={this.props.onYearChange} type="range" name="year" className="form-control-range" min="2010" max="2016" />
+					<output style={{ left: (16.4 * (this.props.year - 2010)) + '%'}} for="year" onforminput="value = year.valueAsNumber;">{this.props.year}</output>
 				</div>
 				<div className="spacing-h"></div>
-				<span className="label large color-dark">2017</span>
+				<span className="label large color-dark">2016</span>
 			</div>
 		);
 	}
